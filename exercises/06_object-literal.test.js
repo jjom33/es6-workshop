de16dd303df46b30ba89059b85754db37929c5d2
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-13 02:22:20 +0900

    finished exercise6

--- exercises/06_object-literal.test.js
+++ exercises/06_object-literal.test.js
@@ -1,14 +1,14 @@
 test('can use shorthand for property names', () => {
   function createMonster(name, power) {
     // Using NEW Object Literal Syntax, return a literal that will allow the tests to pass
-    // return {
-    //   type: 'Monster',
-    //   name: name,
-    //   power: power,
-    //   attack: function (target){
-    //     return `${this.name} attacked ${target.name}`;
-    //   }
-    // }
+    return {
+      type: 'Monster',
+      name: name,
+      power: power,
+      attack: function (target){
+        return `${this.name} attacked ${target.name}`;
+      }
+    }
   }
 
   const godzilla = createMonster('Godzilla', 1000)
@@ -20,10 +20,13 @@ test('can use shorthand for property names', () => {
 
 test('can use expressions as property names', () => {
   function createCandy(type, description) {
+    let upperCaseName = type.toUpperCase() + type.length
     return {
       tasty: true,
       type,
       // add a expression as property name where the property name is the given type.toUpperCase() + type.length
+      [upperCaseName]:description
+
       // sound contrived? It is... 😅
     }
   }
@@ -43,7 +46,7 @@ test('can use expressions as property names', () => {
 http://ws.kcd.im/?ws=ES6+and+Beyond&e=Object+Literals&em=
 */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+  const submitted = true // change this when you've submitted!
   expect(true).toBe(submitted)
 })
 ////////////////////////////////
